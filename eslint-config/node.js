5bdc837633ab36bce71584656d589bcbac40afce
Kristian Dupont <kristian@kristiandupont.com>
2023-05-10 06:36:18 +0000

    Update eslint configs

---
+++ eslint-config/node.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: ["node"],
+  extends: ["plugin:node/recommended"],
+  rules: {
+    "unicorn/prefer-node-protocol": "error",
+  },
+};
---
+++ eslint-config/react.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ["react", "react-hooks", "jsx-a11y", "tailwindcss"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+};
