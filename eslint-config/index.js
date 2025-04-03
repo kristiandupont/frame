52f8d359932dc86c6bd775652aa2e3737ddfacee
Kristian Dupont <kristian@kristiandupont.com>
2025-04-03 07:08:16 +0000

    Bump bump

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -1,52 +1,27 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
 
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
 
-  plugins: ["prettier", "simple-import-sort", "ignore-generated", "filenames"],
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    "prettier",
+    "simple-import-sort",
+    "ignore-generated",
+    "filenames",
+  ],
 
   overrides: [
     {
-      files: ["*.js", "*.jsx"],
-      parser: "@babel/eslint-parser",
-      parserOptions: {
-        requireConfigFile: false,
-      },
-      plugins: [],
-      rules: {},
-    },
-    {
-      files: ["*.ts", "*.tsx"],
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint/eslint-plugin"],
-      extends: ["plugin:@typescript-eslint/recommended"],
-      rules: {
-        "no-unused-vars": "off",
-        "no-undef": "off",
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          {
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-            caughtErrorsIgnorePattern: "^_",
-          },
-        ],
-        "@typescript-eslint/prefer-nullish-coalescing": "error",
-        "@typescript-eslint/explicit-module-boundary-types": "error",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/consistent-type-exports": "error",
-        "@typescript-eslint/consistent-type-imports": "error",
-      },
-    },
-    {
-      files: [
-        "*.{spec,test}.{js,jsx,ts,tsx}",
-        "**/__{mocks,tests}__/**/*.{js,jsx,ts,tsx}",
-      ],
+      files: ["*.{spec,test}.{ts,tsx}", "**/__{mocks,tests}__/**/*.{ts,tsx}"],
       rules: {
         "import/no-extraneous-dependencies": "off",
         "import/no-duplicates": "off",
@@ -58,10 +33,6 @@ module.exports = {
   rules: {
     quotes: "off",
     "no-unused-expressions": "error",
-    "no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-    ],
     "no-implicit-coercion": "error",
     "no-unused-labels": "error",
     "no-process-env": "error",
@@ -77,5 +48,21 @@ module.exports = {
     "arrow-body-style": ["error", "as-needed"],
     "prefer-const": "error",
     "filenames/match-exported": "error",
+    "no-unused-vars": "off",
+    "no-undef": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/prefer-nullish-coalescing": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
   },
 };
